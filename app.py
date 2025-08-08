61702cef41757374bd5421a0021c9af9a13bb8b8
Jodyys <pamanherpp@gmail.com>
2025-08-08 23:49:54 +0700

    ubah file app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Docker + GitHub Actions!"
+    return "Hello Ini Aplikasi Sederhanaaa!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
